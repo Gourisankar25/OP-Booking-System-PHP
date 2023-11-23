a91ce7bf776bc83bcc3293a78259bcb0f214cefa
Gourisankar G <kgourisankar01@gmail.com>
2023-11-23 22:46:10 +0530

    just checked the connection in config.php

--- Admin/config.php
+++ Admin/config.php
@@ -1,3 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","root","","dbhospitalop");
+ $con=mysqli_connect("localhost","root","","dbhospitalop");
+// $con=mysqli_connect("sql203.infinityfree.com","if0_35486125","iwjDxRQyaSKr","if0_35486125_dbhospitalop");
 ?>
